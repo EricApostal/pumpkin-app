a5c9b60b38997bc52bf5458661b3de599975361e
EricApostal <ehapostal04@gmail.com>
2024-12-29 19:09:56 -0500

    android log works temp

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:pumpkin_app/src/rust/api/simple.dart';
 import 'package:pumpkin_app/src/rust/frb_generated.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 Future<void> main() async {
   await RustLib.init();
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
-
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    super.initState();
-    greet(name: "e");
-    runPumpkin();
-  }
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('flutter_rust_bridge quickstart')),
-        body: Center(
-          child: Text(
-            'Action: Call Rust `greet("Tom")`\nResult: `${greet(name: "Tom")}`',
-          ),
-        ),
+        appBar: AppBar(title: const Text('Pumpkin MC Manager')),
+        body: const ServerManagerScreen(),
+      ),
+    );
+  }
+}
+
+class ServerManagerScreen extends StatefulWidget {
+  const ServerManagerScreen({super.key});
+
+  @override
+  State<ServerManagerScreen> createState() => _ServerManagerScreenState();
+}
+
+class _ServerManagerScreenState extends State<ServerManagerScreen> {
+  @override
+  void initState() {
+    super.initState();
+    requestPermissions();
+  }
+
+  Future<void> requestPermissions() async {
+    await Permission.storage.request();
+    if (await Permission.storage.request().isGranted) {
+      print('Storage permission granted');
+    } else {
+      print('Storage permission denied');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () async {
+          print("button pressed, calling run pumpkin");
+          runPumpkin();
+        },
+        child: Text('Send a Signal from Dart to Rust'),
       ),
     );
   }
--- rust/src/api/simple.rs
+++ rust/src/api/simple.rs
@@ -1,14 +1,26 @@
-use pumpkin::{self, PumpkinServer};
+use pumpkin::{self, main};
 
 #[flutter_rust_bridge::frb(sync)] // Synchronous mode for simplicity of the demo
 pub fn greet(name: String) -> String {
     format!("Hello, {name}!")
 }
 
+// #[flutter_rust_bridge::frb()]
 pub async fn run_pumpkin() {
-    let server = PumpkinServer::new().await.unwrap();
-    server.start().await.unwrap();
+    print!("Starting Pumpkin server...");
+    main();
+    // tokio::spawn(async {
+    //     main().await;
+    // });
+    // let server = PumpkinServer::new().await.unwrap();
+    // println!("Pumpkin server started!");
+    // server.start().await.unwrap();
 }
+
+// pub async fn send_command(command: String) {
+//     let server = PumpkinServer::new().await.unwrap();
+//     server.send_command(command).await.unwrap();
+// }
 
 #[flutter_rust_bridge::frb(init)]
 pub fn init_app() {
