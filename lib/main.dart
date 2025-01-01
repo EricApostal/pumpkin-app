5d33ef47aeb4a365b895f8c29c1391bf60d11228
EricApostal <ehapostal04@gmail.com>
2025-01-01 16:20:27 -0500

    add command support

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.dark(),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -33,37 +33,72 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   String outputText = '';
   static const platform = MethodChannel('app_channel');
+  Process? process;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            OutlinedButton(
-              onPressed: () async {
-                final String nativeLibDir =
-                    await platform.invokeMethod('getNativeLibDir');
-                final applicationDirectory =
-                    await getApplicationDocumentsDirectory();
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: MediaQuery.of(context).padding.top + 12,
+            bottom: MediaQuery.of(context).padding.bottom + 12,
+            left: 12,
+            right: 12,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              OutlinedButton(
+                onPressed: () async {
+                  final String nativeLibDir =
+                      await platform.invokeMethod('getNativeLibDir');
+                  final applicationDirectory =
+                      await getApplicationDocumentsDirectory();
 
-                print("Native Library Directory: $nativeLibDir");
-                final executablePath = '$nativeLibDir/libpumpkin.so';
-                final result = await Process.run(executablePath, [],
-                    stdoutEncoding: utf8,
-                    stderrEncoding: utf8,
-                    workingDirectory: applicationDirectory.path);
-                print("Result: ${result.stdout}");
-              },
-              child: const Text('Run Executable'),
-            ),
-            const SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text(outputText),
-            ),
-          ],
+                  print("Native Library Directory: $nativeLibDir");
+                  final executablePath = '$nativeLibDir/libpumpkin.so';
+                  process = await Process.start(
+                    executablePath,
+                    [],
+                    workingDirectory: applicationDirectory.path,
+                  );
+                  process!.stdout.transform(utf8.decoder).listen((data) {
+                    setState(() {
+                      outputText += data;
+                    });
+                  });
+                  process!.stderr.transform(utf8.decoder).listen((data) {
+                    setState(() {
+                      outputText += data;
+                    });
+                  });
+                },
+                child: const Text('Run Executable'),
+              ),
+              const SizedBox(height: 20),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SingleChildScrollView(child: Text(outputText)),
+                ),
+              ),
+              // text box
+              TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Run Command',
+                ),
+                onSubmitted: (value) {
+                  print("submitted: $value");
+                  process?.stdin.writeln(value);
+                },
+                onEditingComplete: () {
+                  print("editing complete");
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
